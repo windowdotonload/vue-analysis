8ecb6a2267691dba6951ed7dcc57f7ea430c16d0
rui.chen <rui.chen@inossem.com>
2023-10-07 13:07:54 +0800

    modify

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:ustbhuangyi/vue-analysis.git master:gh-pages
+git push -f git@github.com:windowdotonload/vue-analysis.git master:gh-pages
 
 cd -
